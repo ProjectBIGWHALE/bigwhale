3ade020762da735ae5919d57d19e747acf74734b
Syllas Braga <syllasbraga2@gmail.com>
2023-08-01 16:54:21 -0300

    feat: Input of images from html

--- src/main/java/com/whale/web/text/extract/controller/TextExtractController.java
+++ src/main/java/com/whale/web/text/extract/controller/TextExtractController.java
@@ -1,14 +1,16 @@
 package com.whale.web.text.extract.controller;
 
 import com.whale.web.text.extract.service.TextExtractService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-@RequestMapping(path = "text-extract")
+@Controller
+@RequestMapping(path = "/text-extract")
 public class TextExtractController {
 
     private final TextExtractService textService;
@@ -17,8 +19,15 @@ public class TextExtractController {
         this.textService = textService;
     }
 
+    @GetMapping
+    public String hello(){
+        return "text-extract";
+    }
+
     @PostMapping
-    public String extractFromImage(@RequestParam MultipartFile file){
-        return textService.extractTextFromImage(file);
+    public String extractFromImage(@RequestParam("file") MultipartFile fileModel, Model model){
+        String extractedText = textService.extractTextFromImage(fileModel);
+        model.addAttribute("extractedText", extractedText);
+        return "text-extracted";
     }
 }
